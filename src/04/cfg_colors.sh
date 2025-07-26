f4f12e528d654cbbdab8f90c8616fda1afbde235
set6am <set6am@yandex.ru>
2025-07-26 21:45:37 +0500

    реализовал функции по работе с конфигом в cfg_colors.sh

---
+++ src/04/cfg_colors.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# глобальные переменные для использования в main.sh
+declare -A color_cfg_dict
+default_colors=0
+
+# функция переноса записей cfg файла в словарь (ассоциативный массив)
+function cfg_colors__convert_to_dict {
+    while IFS='=' read -r key value; do
+        color_cfg_dict["$key"]=$value
+    done < color_set.cfg
+}
+
+# валидация на пустые значения в конфиге
+function cfg_colors__check_empty_values {
+    for value in ${color_cfg_dict[@]}; do
+        if [[ $value == '' ]]; then
+            color_cfg_dict[column1_background]=6 # black
+            color_cfg_dict[column1_font_color]=1 # white
+            color_cfg_dict[column2_background]=2 # red
+            color_cfg_dict[column2_font_color]=4 # blue
+
+            default_colors=1
+            break
+        fi
+}
+
