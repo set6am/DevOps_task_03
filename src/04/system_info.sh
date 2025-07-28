2748e2cfe6e1677188b8bc0ab3e6e617af264499
set6am <set6am@yandex.ru>
2025-07-28 20:48:52 +0500

    реализовал функцию получения системной инфы

--- src/04/system_info.sh
+++ src/04/system_info.sh
@@ -1,27 +1,34 @@
-HOSTNAME=$(hostname)
-TIMEZONE=$(cat /etc/timezone)
-UTC_SHIFT=$(date +%Z | sed -E 's/([+-])0?([0-9]{1,2})/\1\2/')
-OS_INFO=$(lsb_release -d -s)
-DATE=$(date +"%d %b %Y %T")
-UPTIME=$(uptime -p)
-UPTIME_SECONDS=$(cat /proc/uptime | awk '{print $1}')
-IP=$(hostname -I | awk '{print $1}')
-MASK=$(ipcalc $IP -b | grep "Netmask:" | awk '{print $2}')
-GATEWAY=$(ip r | grep "default" | awk '{print $3}')
-RAM_TOTAL_MB=$(free --mega | grep "Mem" | awk '{print $2}')
-RAM_TOTAL="$(( $RAM_TOTAL_MB / 1000 )).$(( $RAM_TOTAL_MB % 1000 )) GB"
-RAM_USED_MB=$(free --mega | grep "Mem" | awk '{print $3}')
-RAM_USED="$(( $RAM_USED_MB / 1000 )).$(( $RAM_USED_MB % 1000 )) GB"
-RAM_FREE_MB=$(free --mega | grep "Mem" | awk '{print $4}')
-RAM_FREE="$(( $RAM_FREE_MB / 1000 )).$(( $RAM_FREE_MB % 1000 )) GB"
-SPACE_ROOT_KB=$(df / | grep "/" | awk '{print $2}')
-SPACE_ROOT="$(( $SPACE_ROOT_KB / 1024 )).$(( $SPACE_ROOT_KB % 1024 / 10 )) MB"
-SPACE_ROOT_USED_KB=$(df / | grep "/" | awk '{print $3}')
-SPACE_ROOT_USED="$(( $SPACE_ROOT_USED_KB / 1024 )).$(( $SPACE_ROOT_USED_KB % 1024 / 10 )) MB"
-SPACE_ROOT_FREE_KB=$(df / | grep "/" | awk '{print $4}')
-SPACE_ROOT_FREE="$(( $SPACE_ROOT_FREE_KB / 1024 )).$(( $SPACE_ROOT_FREE_KB % 1024 / 10 )) MB"
+#!/usr/bin/env bash
 
-OUTPUT="HOSTNAME = $HOSTNAME
+# глобальные переменные для использования в main.sh
+system_info=""
+
+# функция получения информации о системе
+function system_info__get{
+local HOSTNAME=$(hostname)
+local TIMEZONE=$(cat /etc/timezone)
+local UTC_SHIFT=$(date +%Z | sed -E 's/([+-])0?([0-9]{1,2})/\1\2/')
+local OS_INFO=$(lsb_release -d -s)
+local DATE=$(date +"%d %b %Y %T")
+local UPTIME=$(uptime -p)
+local UPTIME_SECONDS=$(cat /proc/uptime | awk '{print $1}')
+local IP=$(hostname -I | awk '{print $1}')
+local MASK=$(ipcalc $IP -b | grep "Netmask:" | awk '{print $2}')
+local GATEWAY=$(ip r | grep "default" | awk '{print $3}')
+local RAM_TOTAL_MB=$(free --mega | grep "Mem" | awk '{print $2}')
+local RAM_TOTAL="$(( $RAM_TOTAL_MB / 1000 )).$(( $RAM_TOTAL_MB % 1000 )) GB"
+local RAM_USED_MB=$(free --mega | grep "Mem" | awk '{print $3}')
+local RAM_USED="$(( $RAM_USED_MB / 1000 )).$(( $RAM_USED_MB % 1000 )) GB"
+local RAM_FREE_MB=$(free --mega | grep "Mem" | awk '{print $4}')
+local RAM_FREE="$(( $RAM_FREE_MB / 1000 )).$(( $RAM_FREE_MB % 1000 )) GB"
+local SPACE_ROOT_KB=$(df / | grep "/" | awk '{print $2}')
+local SPACE_ROOT="$(( $SPACE_ROOT_KB / 1024 )).$(( $SPACE_ROOT_KB % 1024 / 10 )) MB"
+local SPACE_ROOT_USED_KB=$(df / | grep "/" | awk '{print $3}')
+local SPACE_ROOT_USED="$(( $SPACE_ROOT_USED_KB / 1024 )).$(( $SPACE_ROOT_USED_KB % 1024 / 10 )) MB"
+local SPACE_ROOT_FREE_KB=$(df / | grep "/" | awk '{print $4}')
+local SPACE_ROOT_FREE="$(( $SPACE_ROOT_FREE_KB / 1024 )).$(( $SPACE_ROOT_FREE_KB % 1024 / 10 )) MB"
+
+system_info="HOSTNAME = $HOSTNAME
 TIMEZONE = $TIMEZONE UTC $UTC_SHIFT
 USER = $USER
 OS = $OS_INFO
@@ -37,3 +44,4 @@ RAM_FREE = $RAM_FREE
 SPACE_ROOT = $SPACE_ROOT
 SPACE_ROOT_USED = $SPACE_ROOT_USED
 SPACE_ROOT_FREE = $SPACE_ROOT_FREE"
+}
