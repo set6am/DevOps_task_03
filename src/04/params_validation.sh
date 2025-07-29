336da928ab1b7b428cab77a2368715b45d421f66
set6am <set6am@yandex.ru>
2025-07-29 21:17:20 +0500

    рефактор и фиксы функции валидации для работы с ассоциативным массивом

--- src/04/params_validation.sh
+++ src/04/params_validation.sh
@@ -1,24 +1,40 @@
+#!/usr/bin/env bash
 
-function params_validation {
+# функция валидирует номера цветов из конфига
+function params_validation__get_flag {
+    local validation_flag=0
 
-    local -n input_params=$1
-    local -n param1=$2
-    local -n param2=$3
-    local -n param3=$4
-    local -n param4=$5
-    
+    # использую ссылку на массив, чтобы наглядно передавать переменную из другого файла
+    # в функкцию для лучшей читаемости
+    local -n used_colors_nums=$1
 
-    local VALIDATION_FLAG=0
+    local colors_keys=(
+        "column1_background"
+        "column1_font_color"
+        "column2_background"
+        "column2_font_color"
+    )
 
-    for param in "${input_params[@]}"; do
-        if [[ ! $param =~ ^[1-6]$ ]]; then
-            (( VALIDATION_FLAG++ ))
+    # проверка на ввод цифр от 1 до 6
+    for key in "${colors_keys[@]}"; do
+        if [[ ! "${used_colors_nums[$key]}" =~ ^[1-6]$ ]]; then
+            (( validation_flag++ ))
         fi
     done
 
-    if [[ $param1 -eq $param2 || $param3 -eq $param4 ]]; then
-        (( VALIDATION_FLAG++ ))
-    fi
+    bg_fg_pairs=(
+        "column1_background column1_font_color"
+        "column2_background column2_font_color"
+    )
+    
+    # проверка на совпадение цветов текста и фона
+    for pair in "${bg_fg_pairs[@]}"; do
+        read -r bg fg <<< "$pair"
+        if [[ "${used_colors_nums[$bg]}" == "${used_colors_nums[$fg]}" ]]; then
+            (( validation_flag++ ))
+        fi
+    done
 
-    return $VALIDATION_FLAG
+    return $validation_flag
 }
+
