b37be30edc91b96bc569904063f02210a33fa4d9
set6am <set6am@yandex.ru>
2025-07-30 21:39:10 +0500

    добавил функцию вывода инфы о цветах

--- src/04/color_attrs.sh
+++ src/04/color_attrs.sh
@@ -32,3 +32,20 @@ function colors_attrs__get {
         selected_colors_name+=("${colors_name["${used_colors_nums[$key]}"]}")
     done
 }
+
+function colors_attrs__output {
+    local -n default_colors_flag=$1
+    local -n used_colors_nums=$2
+
+    if [[ default_colors_flag -eq 1 ]]; then
+        echo "Column 1 background = default (${selected_colors_name[0]})"
+        echo "Column 1 font color = default (${selected_colors_name[1]})"
+        echo "Column 2 background = default (${selected_colors_name[2]})"
+        echo "Column 2 font color = default (${selected_colors_name[3]})"
+    else
+        echo "Column 1 background = ${used_colors_nums[column1_background]} (${selected_colors_name[0]})"
+        echo "Column 1 font color = ${used_colors_nums[column1_font_color]} (${selected_colors_name[1]})"
+        echo "Column 2 background = ${used_colors_nums[column2_background]} (${selected_colors_name[2]})"
+        echo "Column 2 font color = ${used_colors_nums[column2_font_color]} (${selected_colors_name[3]})"
+    fi
+}
